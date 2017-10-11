5d422f6e90e94fcaa73e8b4b3daa2f958d5c61ef
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-10-11 10:08:35 +0200

    ir done

--- vhdl/IR.vhd
+++ vhdl/IR.vhd
@@ -11,5 +11,23 @@ entity IR is
 end IR;
 
 architecture synth of IR is
+
+  signal s_D : in std_logic_vector(31 downto 0);
+
 begin
+
+pro_save : process(D, enable)
+begin
+  if (enable = '1') then
+    s_D <= D;
+  end if;
+end process;
+
+pro_write : process(clk)
+begin
+  if(rising_edge(clk)) then
+    Q <= s_D;
+  end if;
+end process;
+
 end synth;
