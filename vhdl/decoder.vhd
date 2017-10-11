2ee6337366bc6cf126340c407fe2c8b09418d156
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-10-11 09:17:54 +0200

    extend decoder with cs_buttons

--- vhdl/decoder.vhd
+++ vhdl/decoder.vhd
@@ -7,7 +7,8 @@ entity decoder is
     address : in  std_logic_vector(15 downto 0);
     cs_LEDS : out std_logic;
     cs_RAM  : out std_logic;
-    cs_ROM  : out std_logic
+    cs_ROM  : out std_logic;
+    cs_buttons : out std_logic
   );
 end decoder;
 
@@ -19,19 +20,28 @@ begin
 		if(unsigned(address) >= X"0000" and unsigned(address) <= X"0FFC") then
 			cs_RAM <= '0';
 			cs_LEDS <= '0';
+      cs_buttons <= '0';
 			cs_ROM <= '1';
 		elsif(unsigned(address) >= X"1000" and unsigned(address) <= X"1FFC") then
 			cs_ROM <= '0';
 			cs_LEDS <= '0';
+      cs_buttons <= '0';
 			cs_RAM <= '1';
 		elsif(unsigned(address) >= X"2000" and unsigned(address) <= X"200C") then
 			cs_ROM <= '0';
 			cs_RAM <= '0';
+      cs_buttons <= '0';
 			cs_LEDS <= '1';
+    elsif(unsigned(address) >= X"2030" and unsigned(address) <= X"2034") then
+      cs_ROM <= '0';
+			cs_RAM <= '0';
+      cs_LEDS <= '0';
+      cs_buttons <= '1';
 		else
 			cs_ROM <= '0';
 			cs_RAM <= '0';
 			cs_LEDS <= '0';
+      cs_buttons <= '0';
 		end if;
 	end process select_slave;
 end synth;
